78b1192866edb81092eb0e3c34220aaaf048d459
Enzo Weder <enzowederma@gmail.com>
2023-06-10 19:18:47 -0300

    ✨ feat: add SpaceShip class

---
+++ TrabFinal/src/Spaceship.java
@@ -0,0 +1,15 @@
+public abstract class Spaceship {
+    private String nome;
+    private String modelo;
+    private String aparicao;
+
+    public String getNome(){ return this.nome; }
+    public void setNome(String arg){ this.nome = arg; }
+
+    public String getModelo(){ return this.modelo; }
+    public void setModelo(String arg){ this.modelo = arg; }
+
+    public String getAparicao(){ return this.aparicao; }
+    public void setAparicao(String arg){ this.aparicao = arg; }
+
+}
