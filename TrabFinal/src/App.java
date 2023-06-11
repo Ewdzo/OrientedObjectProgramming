bed308951e067fd8981d4bd41cee2a018918722b
Enzo Weder <enzowederma@gmail.com>
2023-06-11 17:05:31 -0300

    ✨ feat: add loadPlanets from .ser feature

--- TrabFinal/src/App.java
+++ TrabFinal/src/App.java
@@ -1,8 +1,11 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import javax.swing.JButton;
@@ -15,7 +18,7 @@ public class App extends JFrame{
 
     App(){
         App frame = this;
-        JButton createSpaceshipButton, createPlanetButton, showPlanetsButton, showSpaceshipsButton, savePlanetsButton, saveSpaceshipsButton;
+        JButton createSpaceshipButton, createPlanetButton, showPlanetsButton, showSpaceshipsButton, savePlanetsButton, saveSpaceshipsButton, loadPlanetsButton, loadSpaceshipsButton;
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLayout(null);
 
@@ -98,6 +101,31 @@ public class App extends JFrame{
             }
         }); 
         this.add(saveSpaceshipsButton);
+
+        loadPlanetsButton = new JButton("Carregar Planetas");
+        loadPlanetsButton.setBounds(15, 375, 250, 50);
+        loadPlanetsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                try{
+                    FileInputStream fs = new FileInputStream("Planets.ser");
+                    ObjectInputStream os = new ObjectInputStream(fs);
+                    
+                    while(true){
+                        try {
+                            Object restoredObject = os.readObject();
+                            Planet restoredPlanet = (Planet) restoredObject;
+                            planets.add(restoredPlanet);
+                        } 
+                        catch (EOFException e1){ os.close(); }
+                        catch (Exception e2){ e2.printStackTrace(); }
+
+                    }
+                
+                }
+                catch (IOException e3) { e3.printStackTrace(); }
+            }
+        }); 
+        this.add(loadPlanetsButton);
 
         this.setPreferredSize( new Dimension(300, 600) );
         this.pack();
