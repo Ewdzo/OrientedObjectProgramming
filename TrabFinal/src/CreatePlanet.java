09daa10d36d7be6552f0a17f7c93813c21b90e27
Enzo Weder <enzowederma@gmail.com>
2023-06-10 21:25:09 -0300

    ✨ feat: add button into createPlanet gui

--- TrabFinal/src/CreatePlanet.java
+++ TrabFinal/src/CreatePlanet.java
@@ -1,23 +1,27 @@
 import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Label;
 
 
 public class CreatePlanet extends JFrame {
     TextField nomeTextField, descricaoTextField, aparicaoTextField;
     Label tituloLabel, nomeLabel, descricaoLabel, aparicaoLabel;
+    JButton createPlanetButton;
     
     CreatePlanet(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
 
         tituloLabel = new Label("Cadastrar Novo Planeta");
-        tituloLabel.setBounds(60, 0, 250, 15);
+        tituloLabel.setBounds(70, 0, 250, 15);
         this.add(tituloLabel);
 
-        nomeLabel = new Label();
-		nomeLabel.setText("Nome do Planeta");
+        nomeLabel = new Label("Nome do Planeta");
         nomeLabel.setBounds(10, 30, 250, 15);
         this.add(nomeLabel);
 
@@ -25,8 +29,7 @@ public class CreatePlanet extends JFrame {
         nomeTextField.setBounds(10, 50, 250, 30);
         this.add(nomeTextField);
 
-        descricaoLabel = new Label();
-		descricaoLabel.setText("Descrição do Planeta");
+        descricaoLabel = new Label("Descrição do Planeta");
         descricaoLabel.setBounds(10, 100, 250, 15);
         this.add(descricaoLabel);
 
@@ -34,8 +37,7 @@ public class CreatePlanet extends JFrame {
         descricaoTextField.setBounds(10, 120, 250, 100);
         this.add(descricaoTextField);
 
-        aparicaoLabel = new Label();
-		aparicaoLabel.setText("Primeira Aparição do Planeta");
+        aparicaoLabel = new Label("Primeira Aparição do Planeta");
         aparicaoLabel.setBounds(10, 235, 250, 15);
         this.add(aparicaoLabel);
 
@@ -43,7 +45,17 @@ public class CreatePlanet extends JFrame {
         aparicaoTextField.setBounds(10, 255, 250, 30);
         this.add(aparicaoTextField);
 
-        this.setPreferredSize( new Dimension(300, 350) );
+        createPlanetButton = new JButton("Criar Planeta");
+        createPlanetButton.setBounds(60, 300, 150, 30);
+        createPlanetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Hi");
+            }
+        }); 
+        this.add(createPlanetButton);
+
+
+        this.setPreferredSize( new Dimension(300, 400) );
         this.pack();
 		this.setVisible(true);
     }
